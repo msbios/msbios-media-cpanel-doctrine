416361fa23acef22fad8421406a07be1d26c10fb
Judzhin Miles <info@msbios.com>
2018-12-12 12:59:30 +0200

    Lazy Composer Commiter

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -7,9 +7,11 @@
 namespace MSBios\Media\CPanel\Doctrine\Controller;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
 use MSBios\CPanel\Doctrine\Mvc\Controller\AbstractActionController;
 use MSBios\Media\CPanel\Doctrine\Filter\NewsImageChain;
 use MSBios\Media\Resource\Doctrine\Entity\News;
+use MSBios\Paginator\Doctrine\Adapter\QueryBuilderPaginator;
 use Zend\Filter\FilterInterface;
 use Zend\Filter\FilterPluginManager;
 use Zend\Form\FormInterface;
@@ -34,6 +36,27 @@ class NewsController extends AbstractActionController
     {
         parent::__construct($dem, $form);
         $this->filterPluginManager = $filterPluginManager;
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     */
+    public function __invoke(QueryBuilder $queryBuilder)
+    {
+
+        if ($this->search->isValid())
+        {
+            /** @var array $data */
+            $data = $this->search->getData();
+
+            if (!empty($data['q'])) {
+                $queryBuilder->where($queryBuilder->expr()->like('n.title', ':name'))
+                ->setParameter('name', "{$data['q']}%");
+            }
+
+        }
+
+        return new QueryBuilderPaginator($queryBuilder);
     }
 
     /**
